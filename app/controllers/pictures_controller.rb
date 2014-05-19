054c9d0e9328060881f3522b4a06a81a3992648b
jmontuiuc <j-monton@kellogg.northwestern.edu>
2014-05-18 19:36:20 -0500

    Completed Part I

--- app/controllers/pictures_controller.rb
+++ app/controllers/pictures_controller.rb
@@ -1,4 +1,17 @@
 class PicturesController < ApplicationController
   def show
+    @picture = Picture.find_by({ :id => params[:id] })
+  end
+
+  def index
+    @allpic = Picture.all
+  end
+
+  def new
+
+  end
+
+  def create
+
   end
 end
